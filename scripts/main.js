048cc425be15f71ee74e6998f6225b605027ecbc
unknown <handemore7@gmail.com>
2020-03-13 08:10:45 -0500

    El texto mostrado queda en mayusculas y se borra el contenido al des-seleccionar el input

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,21 @@
+/* 
+    Al escribir en el input se cambia el contenido del texto por el del input
+*/
 var contentShowed = document.querySelector('.content__showed');
 var contentInserted = document.querySelector('.content__inserted');
 console.log(contentShowed);
 
 var handleChangeContent = function(event) {
-    contentShowed.textContent= asdcontentInserted.value;
+    contentShowed.textContent= contentInserted.value.toUpperCase();
 }
 
 contentInserted.addEventListener('input', handleChangeContent);
+
+/* 
+    Al clickear por fuera del input se borra el contenido
+*/
+var handleClearContent = function(event){
+    contentInserted.value = "";
+}
+
+contentInserted.addEventListener('change', handleClearContent)
